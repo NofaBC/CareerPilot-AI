fcb3cd35a7bd83f5cb0f5522c8d63783df633a44
NofaBC <167025181+NofaBC@users.noreply.github.com>
2026-01-19 11:41:03 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,17 @@
-/** @type {import("next").NextConfig} */ const nextConfig = { experimental: { serverComponentsExternalPackages: ["firebase-admin"] }, env: { NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN, NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID } }; export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
